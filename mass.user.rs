45ab315d2884a3e15e7e8673f41c0e992f0e53f0
Nikola Babic <bbcnkl@gmail.com>
2017-10-21 12:11:36 +0200

    Create mass.user.rs

---
+++ mass.user.rs
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Agar Balkan Mass - Q
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+ // @match       http://www.agar.rs/*
+ // @match       http://agar.rs/*
+// @grant        none
+// ==/UserScript==
+window.addEventListener('load', function() {
+    console.log('called');
+    var interval;
+    var switchy = false;
+    $(document).on('keydown',function(e){
+        console.log('keydown e.keyCode="'+e.keyCode+'"');
+        if(e.keyCode == 81){
+            console.log('keydown 81, switchy '+switchy);
+            if(switchy){
+                return;
+            }
+            switchy = true;
+            interval = setInterval(function() {
+                console.log('firing');
+                $("body").trigger($.Event("keydown", { keyCode: 87}));
+                $("body").trigger($.Event("keyup", { keyCode: 87}));
+            }, 10);//increase this number to make it fire them out slower
+        }
+    })
+
+    $(document).on('keyup',function(e){
+        console.log('keyup e.keyCode="'+e.keyCode+'"');
+        if(e.keyCode == 81){
+            console.log('stop firing');
+            switchy = false;
+            clearInterval(interval);
+            return;
+        }
+    })
+}, true)
