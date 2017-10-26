1babf814134d4fd61ac918ee05a2a5254230d41e
Nikola Babic <bbcnkl@gmail.com>
2017-10-26 22:55:10 +0200

    Update masseject.user.js

--- masseject.user.js
+++ masseject.user.js
@@ -15,8 +15,7 @@ window.addEventListener('load', function() {
     var switchy = false;
     $(document).on('keydown',function(e){
         console.log('keydown e.keyCode="'+e.keyCode+'"');
-        if(e.keyCode == 81){
-            console.log('keydown 81, switchy '+switchy);
+        if(e.keyCode == 69){
             if(switchy){
                 return;
             }
@@ -31,7 +30,7 @@ window.addEventListener('load', function() {
 
     $(document).on('keyup',function(e){
         console.log('keyup e.keyCode="'+e.keyCode+'"');
-        if(e.keyCode == 81){
+        if(e.keyCode == 69){
             console.log('stop firing');
             switchy = false;
             clearInterval(interval);
